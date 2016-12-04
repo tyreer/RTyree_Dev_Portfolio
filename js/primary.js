d40bab466fef5943ef360d97740772fe82574b9c
Robert Tyree <tyreer@gmail.com>
2016-12-04 15:59:20 -0800

    Centered nav bar icon on smaller browsers. Added custom cursor to nav to assist UI. Made nav options full screen. Added click event handlers on nav anchor items.

--- js/primary.js
+++ js/primary.js
@@ -2,10 +2,18 @@
 
 document.getElementById("navBars").addEventListener("click", toggleMobileMenu);
 
+//Is it awful to declare a global variable here?
+const menuAnchors = document.getElementsByClassName("menuAnchor");
+
+//Could I use map here, or would that require Babel?
+for (let x = 0; x<menuAnchors.length; x++){
+  menuAnchors[x].addEventListener("click", toggleMobileMenu);
+}
+
 function toggleMobileMenu () {
-  if (document.getElementById("navOptions").className === "navOptions navDisplayToggleOff"){
+  if (document.getElementById("navOptions").className === "navDisplayToggleOff"){
     document.getElementById("navOptions").className = "navDisplayToggleOn";
   } else {
-    document.getElementById("navOptions").className = "navOptions navDisplayToggleOff";
+    document.getElementById("navOptions").className = "navDisplayToggleOff";
   }
 }
