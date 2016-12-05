bcf8eef7e03f2dd8668443c1b9b217d2f73abc02
Robert Tyree <tyreer@gmail.com>
2016-12-04 21:18:40 -0800

    Added opacity to nav. Disabled scrolling when nav is fullscreen. Added textContent substitution on nav icon. Changed cursor types. Added intro headerBob animation loop.

--- js/primary.js
+++ js/primary.js
@@ -13,7 +13,14 @@ for (let x = 0; x<menuAnchors.length; x++){
 function toggleMobileMenu () {
   if (document.getElementById("navOptions").className === "navDisplayToggleOff"){
     document.getElementById("navOptions").className = "navDisplayToggleOn";
+    document.getElementById("scrollControl").className = "disableScroll";
+    document.getElementById("navBars").textContent = "✕";
+
   } else {
     document.getElementById("navOptions").className = "navDisplayToggleOff";
+    document.getElementById("scrollControl").className = "";
+    document.getElementById("navBars").textContent = "☰";
   }
+
+
 }
