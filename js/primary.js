ff4bda7a4967498ffa317a9e6756cfd3dcc7c8d2
Robert Tyree <tyreer@gmail.com>
2016-12-05 19:22:16 -0800

    Added touchstart and touchmove event handlers to disable scroll in nav full-screen mode.

--- js/primary.js
+++ js/primary.js
@@ -10,16 +10,28 @@ for (let x = 0; x<menuAnchors.length; x++){
   menuAnchors[x].addEventListener("click", toggleMobileMenu);
 }
 
+function touchstart(e) {
+    e.preventDefault()
+}
+
+function touchmove(e) {
+    e.preventDefault()
+}
+
 function toggleMobileMenu () {
   if (document.getElementById("navOptions").className === "navDisplayToggleOff"){
     document.getElementById("navOptions").className = "navDisplayToggleOn";
     document.getElementById("scrollControl").className = "disableScroll";
     document.getElementById("navBars").textContent = "✕";
+    document.addEventListener('touchstart', this.touchstart);
+    document.addEventListener('touchmove', this.touchmove);
 
   } else {
     document.getElementById("navOptions").className = "navDisplayToggleOff";
     document.getElementById("scrollControl").className = "";
     document.getElementById("navBars").textContent = "☰";
+    document.removeEventListener('touchstart', this.touchstart);
+    document.removeEventListener('touchmove', this.touchmove);
   }
 
 
