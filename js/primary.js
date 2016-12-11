b8be85f2016ae46510312f21f1a185e6fe929bf7
Robert Tyree <tyreer@gmail.com>
2016-12-11 11:25:53 -0800

    Modified nav display to improve overlay on full screen mode. Distinguished between 100vh navContainer and 70vh navOptions div ids.

--- js/primary.js
+++ js/primary.js
@@ -14,11 +14,13 @@ function toggleMobileMenu () {
   if (document.getElementById("navOptions").className === "navDisplayToggleOff"){
     document.getElementById("main-nav").className = "main-nav-opened";
     document.getElementById("navOptions").className = "navDisplayToggleOn";
+    document.getElementById("navContainer").className = "navContainerToggleOn";
     document.getElementById("scrollControl").className = "disable-scroll";
     document.getElementById("navBars").textContent = "✕";
   } else {
     document.getElementById("navOptions").className = "navDisplayToggleOff";
     document.getElementById("main-nav").className = "main-nav";
+    document.getElementById("navContainer").className = "navContainer";
     document.getElementById("scrollControl").className = "";
     document.getElementById("navBars").textContent = "☰";
   }
