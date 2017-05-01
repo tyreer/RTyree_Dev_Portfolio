4a5c79b17d5841bf70f319dd0220609962216b8e
Robert Tyree <tyreer@gmail.com>
2017-05-01 19:16:45 +0100

    Replaced all camelcase from ID and class selectors with hyphen naming. Broke Sass into component specific partials.

--- js/primary.babel.js
+++ js/primary.babel.js
@@ -2,24 +2,24 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-document.getElementById("navBars").addEventListener("click", toggleMobileMenu);
+document.getElementById("nav-bars").addEventListener("click", toggleMobileMenu);
 
-[].concat(_toConsumableArray(document.getElementsByClassName("menuAnchor"))).forEach(function (element) {
+[].concat(_toConsumableArray(document.getElementsByClassName("menu-anchor"))).forEach(function (element) {
   return element.addEventListener("click", toggleMobileMenu);
 });
 
 function toggleMobileMenu() {
-  if (document.getElementById("navOptions").className === "navDisplayToggleOff") {
+  if (document.getElementById("nav-options").className === "nav-display-toggle-off") {
     document.getElementById("main-nav").className = "main-nav-opened";
-    document.getElementById("navOptions").className = "navDisplayToggleOn";
-    document.getElementById("navContainer").className = "navContainerToggleOn";
-    document.getElementById("scrollControl").className = "disable-scroll";
-    document.getElementById("navBars").textContent = "✕";
+    document.getElementById("nav-options").className = "nav-display-toggle-on";
+    document.getElementById("nav-container").className = "nav-container-toggle-on";
+    document.getElementById("scroll-control").className = "disable-scroll";
+    document.getElementById("nav-bars").textContent = "✕";
   } else {
-    document.getElementById("navOptions").className = "navDisplayToggleOff";
+    document.getElementById("nav-options").className = "nav-display-toggle-off";
     document.getElementById("main-nav").className = "main-nav";
-    document.getElementById("navContainer").className = "navContainer";
-    document.getElementById("scrollControl").className = "";
-    document.getElementById("navBars").textContent = "☰";
+    document.getElementById("nav-container").className = "nav-container";
+    document.getElementById("scroll-control").className = "";
+    document.getElementById("nav-bars").textContent = "☰";
   }
 };
