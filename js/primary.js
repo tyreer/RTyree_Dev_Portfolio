dc37449b1792d2045eddfdcbc95fddea0c192dc1
Robert Tyree <tyreer@gmail.com>
2017-08-09 23:48:22 +0100

    Replace CV

--- js/primary.js
+++ js/primary.js
@@ -12,17 +12,15 @@ function toggleMobileMenu() {
     document.getElementById("nav-container").className = "nav-container--full-screen";
     document.getElementById("scroll-control").className = "disable-scroll";
     document.getElementById("nav-bars").textContent = "✕";
-
     document.ontouchmove = (event) => {
-    event.preventDefault();
-  }
+      event.preventDefault();
+    }
   } else {
     document.getElementById("nav-options").className = "nav-options--collapsed";
     document.getElementById("main-nav").className = "main-nav--collapsed";
     document.getElementById("nav-container").className = "nav-container--collapsed";
     document.getElementById("scroll-control").className = "";
     document.getElementById("nav-bars").textContent = "☰";
-
     document.ontouchmove = (event) => true;
   }
 };
