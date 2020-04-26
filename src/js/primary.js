90fe6abc976b02a8faee06f56f8347dc0ee40453
Robert Tyree <>
2020-04-26 15:53:58 +0100

    prettier format

--- src/js/primary.js
+++ src/js/primary.js
@@ -1,13 +1,8 @@
 const toggleMobileMenu = () => {
-  if (
-    document.getElementById("nav-options").className ===
-    "nav-options--collapsed"
-  ) {
+  if (document.getElementById("nav-options").className === "nav-options--collapsed") {
     document.getElementById("main-nav").className = "main-nav--full-screen";
-    document.getElementById("nav-options").className =
-      "nav-options--full-screen";
-    document.getElementById("nav-container").className =
-      "nav-container--full-screen";
+    document.getElementById("nav-options").className = "nav-options--full-screen";
+    document.getElementById("nav-container").className = "nav-container--full-screen";
     document.getElementById("scroll-control").className = "disable-scroll";
     document.getElementById("nav-bars").textContent = "✕";
     document.ontouchmove = (event) => {
@@ -16,8 +11,7 @@ const toggleMobileMenu = () => {
   } else {
     document.getElementById("nav-options").className = "nav-options--collapsed";
     document.getElementById("main-nav").className = "main-nav--collapsed";
-    document.getElementById("nav-container").className =
-      "nav-container--collapsed";
+    document.getElementById("nav-container").className = "nav-container--collapsed";
     document.getElementById("scroll-control").className = "";
     document.getElementById("nav-bars").textContent = "☰";
     document.ontouchmove = (event) => !!event;
