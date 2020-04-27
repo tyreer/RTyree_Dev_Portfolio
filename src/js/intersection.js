4a29c8aa2dd6c5510124259b59c2b4ba01d92815
Robert Tyree <>
2020-04-27 08:13:42 +0100

    add intro background bar animation

--- src/js/intersection.js
+++ src/js/intersection.js
@@ -2,9 +2,12 @@ let intersectionCount = 0;
 const bgDivs = document.getElementsByClassName("div-bg");
 
 const topCallback = () => {
-  if (intersectionCount % 2 !== 0) {
-    bgDivs[0].classList.add("div-bg--left");
-    bgDivs[1].classList.add("div-bg--right");
+  if (intersectionCount === 0) {
+    bgDivs[0].classList.remove("div-bg--initial-left");
+    bgDivs[1].classList.remove("div-bg--initial-right");
+  } else if (intersectionCount % 2 !== 0) {
+    bgDivs[0].classList.add("div-bg--left", "div-bg--fast");
+    bgDivs[1].classList.add("div-bg--right", "div-bg--fast");
   } else {
     bgDivs[0].classList.remove("div-bg--left");
     bgDivs[1].classList.remove("div-bg--right");
