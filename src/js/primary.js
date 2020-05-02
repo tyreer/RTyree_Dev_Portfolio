0f23da8f04aa5a07c3711957fe0524bde0037973
Robert Tyree <>
2020-05-02 18:23:24 +0100

    WIP refactor to more semantic nav

--- src/js/primary.js
+++ src/js/primary.js
@@ -1,10 +1,11 @@
 const toggleMobileMenu = () => {
   if (document.getElementById("nav-options").className === "nav-options--collapsed") {
-    document.getElementById("main-nav").className = "main-nav--full-screen";
+    document.getElementById("main-nav").className = "main-nav main-nav--full-screen";
     document.getElementById("nav-options").className = "nav-options--full-screen";
     document.getElementById("nav-container").className = "nav-container--full-screen";
     document.getElementById("scroll-control").className = "disable-scroll";
     document.getElementById("nav-bars").textContent = "✕";
+    document.getElementById("nav-bars").blur();
     document.ontouchmove = (event) => {
       event.preventDefault();
     };
@@ -14,12 +15,13 @@ const toggleMobileMenu = () => {
     document.getElementById("nav-container").className = "nav-container--collapsed";
     document.getElementById("scroll-control").className = "";
     document.getElementById("nav-bars").textContent = "☰";
+    document.getElementById("nav-bars").blur();
     document.ontouchmove = (event) => !!event;
   }
 };
 
 document.getElementById("nav-bars").addEventListener("click", toggleMobileMenu);
 
-[...document.getElementsByClassName("menu-anchor")].forEach((element) =>
+[...document.getElementsByClassName("main-nav__link")].forEach((element) =>
   element.addEventListener("click", toggleMobileMenu)
 );
