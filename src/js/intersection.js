4593bf328521b8fc48c4e894b8ee0201c2ce7889
Robert Tyree <>
2020-05-23 20:17:01 +0100

    add stagered title animation.

--- src/js/intersection.js
+++ src/js/intersection.js
@@ -1,16 +1,18 @@
 let intersectionCount = 0;
 const bgDivs = document.getElementsByClassName("div-bg");
+const title = document.getElementsByClassName("title-container--initial")[0];
 
 const topCallback = () => {
   if (intersectionCount === 0) {
     bgDivs[0].classList.remove("div-bg--initial-left");
     bgDivs[1].classList.remove("div-bg--initial-right");
+    title.classList.remove("title-container--initial");
   } else if (intersectionCount % 2 !== 0) {
-    bgDivs[0].classList.add("div-bg--left", "div-bg--fast");
-    bgDivs[1].classList.add("div-bg--right", "div-bg--fast");
+    bgDivs[0].classList.add("div-bg--wide-left", "div-bg--fast");
+    bgDivs[1].classList.add("div-bg--wide-right", "div-bg--fast");
   } else {
-    bgDivs[0].classList.remove("div-bg--left");
-    bgDivs[1].classList.remove("div-bg--right");
+    bgDivs[0].classList.remove("div-bg--wide-left");
+    bgDivs[1].classList.remove("div-bg--wide-right");
   }
 
   intersectionCount += 1;
