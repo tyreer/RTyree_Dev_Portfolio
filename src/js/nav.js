65f352b3b54fc2da912e24da1a6a0395409c168f
Robert Tyree <20293876+tyreer@users.noreply.github.com>
2020-10-16 10:31:45 +0100

    WIP FP refactor

--- src/js/nav.js
+++ src/js/nav.js
@@ -4,20 +4,38 @@ const navContainer = document.getElementById("nav-container");
 const scrollControl = document.getElementById("scroll-control");
 const navButton = document.getElementById("nav-button");
 
+// const getCollapsed = (element) => (targetClass) => element.className.includes(targetClass);
+
+function getCollapsed({ element, targetClass }) {
+  return element.className.includes(targetClass);
+}
+
+function getClassName({ baseClass, isCollapsed }) {
+  return `${baseClass} ${baseClass}${isCollapsed ? "--full-screen" : "--collapsed"}`;
+}
+
 function toggleMenu() {
-  const isCollapsed = navOptions.className.includes("nav-options--collapsed");
+  // const isCollapsed = getCollapsed(navOptions)("nav-options--collapsed");
+
+  const collapsedIndicator = { element: navOptions, targetClass: "nav-options--collapsed" };
+  const isCollapsed = getCollapsed(collapsedIndicator);
 
   // Toggle classes
-  mainNav.classList.toggle("nav--collapsed");
-  mainNav.classList.toggle("nav--full-screen");
-  navOptions.className = `nav-options${isCollapsed ? "--full-screen" : "--collapsed"}`;
-  navContainer.className = `nav-container nav-container${isCollapsed ? "--full-screen" : "--collapsed"}`;
+  const mainNavConfig = { baseClass: "nav", isCollapsed };
+  const navOptionsConfig = { baseClass: "nav-options", isCollapsed };
+  const navContainerConfig = { baseClass: "nav-container", isCollapsed };
+
+  mainNav.className = getClassName(mainNavConfig);
+  navOptions.className = getClassName(navOptionsConfig);
+  navContainer.className = getClassName(navContainerConfig);
 
   // Manage button
   navButton.blur();
 
   // Disable scroll
   scrollControl.classList.toggle("disable-scroll");
+
+  // Is this needed?
   document.ontouchmove = isCollapsed ? (event) => event.preventDefault() : (event) => !!event;
 
   // Set ARIA attributes
